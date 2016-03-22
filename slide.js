1a9ab6243fcec4610f1161527ff44025e8e6f587
andrew840513 <andrewchen@Andrews-Mac.local>
2016-03-21 20:06:18 -0700

    finish index
    complete the menu and members page+ rwd

---
+++ slide.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+$(document).ready(function(){
+    $("#menu").click(function(){
+        $("#nav_menu").slideToggle("fast");
+    });
+});
+
+if (matchMedia) {
+	var mq = window.matchMedia("(min-width: 1050px)");
+	mq.addListener(WidthChange);
+	WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+
+	if (mq.matches) {
+		$("#nav_menu").css("display","block"); //display block when change from 550px to 551 prevent the menu dispear
+		$("#menu").css("display","none");
+	}
+	else {
+		$("#nav_menu").slideUp(0); //slide up when chenge 551 to 550
+		$("#menu").css("display","block");
+	}
+}
