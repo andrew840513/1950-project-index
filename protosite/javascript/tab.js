f38a53cdb3cd8760f9a02ea319cb7368ec9e770f
Andrew Chen <andrew@mren.com.tw>
2016-03-26 16:30:25 -0700

    add hamburger, add blog link

--- protosite/javascript/tab.js
+++ protosite/javascript/tab.js
@@ -5,3 +5,28 @@ $(function() {
 $(function() {
    $("#accordion").accordion({ collapsible: true, active: false, heightStyle: "content"});
   });
+  
+$(document).ready(function(){
+    $("#menu_btn").click(function(){
+        $("#nav_menu").animate({width:'toggle'},0);
+    });
+});
+
+if (matchMedia) {
+	var mq = window.matchMedia("(min-width: 800px)");
+	mq.addListener(WidthChange);
+	WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+
+	if (mq.matches) {
+		$("#nav_menu").css("display","block"); //display block when change from 550px to 551 prevent the menu dispear
+		$("#menu_btn").css("display","none");
+	}
+	else {
+		$("#nav_menu").slideUp(0); //slide up when chenge 551 to 550
+		$("#menu_btn").css("display","block");
+	}
+}
